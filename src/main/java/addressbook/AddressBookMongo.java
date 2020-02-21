0358b463fbc669b122ddfca568033515f5e60b6e
Zdzichu <Zdzichu@LAPTOP-ATQM63LV>
2020-02-22 00:54:02 +0100

    comit next

--- src/main/java/addressbook/AddressBookMongo.java
+++ src/main/java/addressbook/AddressBookMongo.java
@@ -1,8 +1,27 @@
 package addressbook;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
 
+import java.net.UnknownHostException;
+
+
+
+import java.util.Date;
+
+
+
+
+import com.mongodb.MongoException;
+
+import com.mongodb.*;
+ 
+
+
+
+@SuppressWarnings("deprecation")
 public class AddressBookMongo implements AddressBook {
      String host ="192.168.0.29" ;
      int port = 27017;
@@ -10,16 +29,16 @@ public class AddressBookMongo implements AddressBook {
      String dbUser = "zdzichudb";
      String dbPassword = "kwiatek33";
      
-	
+  
 	
 	public void addEntry(String firstName, String phone) {
 		DBCollection addressCollection = getAddressCollection();
 		
-		address.insert(new BasicDBObject(firstName, phone));
+		addressCollection.insert(new BasicDBObject(firstName, phone));
 	}
 
 	
-
+	
 	public int size() {
 		DBCollection addressCollection = getAddressCollection();
 		return (int) addressCollection.getCount();
@@ -28,17 +47,15 @@ public class AddressBookMongo implements AddressBook {
 	private DBCollection getAddressCollection() {
 		try {
 			Mongo mongo = new Mongo(host , port);
-			DB db = mongo.getDB(dbName);
-			db.authenticate(dbUser,dbPassword.toCharArray());
+			 DB db = mongo.getDB(dbName);
+			 db.authenticate(dbUser,dbPassword.toCharArray());
 			DBCollection address = db.getCollection("addresses");
 			return address;
 		} catch (Exception e) {
 			throw new RuntimeException(e);
-		} catch MongoException e){
-			throw new RuntimeException(e);
 		
-		}
 	}
-	
-	
+	}
 }
+	
+
