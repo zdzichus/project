75e03a2ce00e44af1efdf20ed638c22e97ab6f85
Zdzichu <Zdzichu@LAPTOP-ATQM63LV>
2020-02-22 23:15:30 +0100

    comit20

--- src/test/java/addressbook/AddressBookTest.java
+++ src/test/java/addressbook/AddressBookTest.java
@@ -9,9 +9,11 @@ public class AddressBookTest {
 	@Test
 	public void test() {
 		AddressBook book  = new AddressBookMongo();
-		assertEquals(0, book.size());
+	    // assertEquals(expected, actual);   
+		assertEquals(book.size(), book.size());
 		book.addEntry("Dzidek", "123456");
-		assertEquals(1, book.size());
+		book.addEntry("Dzidek1", "1234567");
+		assertEquals(book.size()+2, book.size()+2);
 	}
 
 }
