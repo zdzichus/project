2fa15a8ef8d71bf269a5846aa9483d4c168ef413
Zdzichu <Zdzichu@LAPTOP-ATQM63LV>
2020-03-14 17:24:07 +0100

    zmiana miejsca addeess book

--- src/main/java/addressbook/AddressBookServlet.java
+++ src/main/java/addressbook/AddressBookServlet.java
@@ -8,13 +8,15 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
  public class AddressBookServlet extends HttpServlet {
-          
+	
+	public static final long serialVersionUID = 1L;
+		AddressBook book = new AddressBookMongo();
          
         public void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         	
         	String firstName = request.getParameter("firstName");
         	String phone = request.getParameter("phone");
-        	AddressBook book = new AddressBookMongo();
+        	
         	book.addEntry(firstName, phone);   	
     
         	response.getWriter().println("Added " + firstName + ", there are now " + book.size() + "entries" );
