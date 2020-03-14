88d9d97efce2cb921c0721223adfd5bd4fb5ba95
Zdzichu <Zdzichu@LAPTOP-ATQM63LV>
2020-03-14 16:57:52 +0100

    clean

--- src/main/java/addressbook/AddressBookServlet.java
+++ src/main/java/addressbook/AddressBookServlet.java
@@ -8,17 +8,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
  public class AddressBookServlet extends HttpServlet {
-         AddressBook book = new AddressBookMongo();
-        
+           AddressBook book = new AddressBookMongo();
+         
         protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         	
         	String firstName = request.getParameter("firstName");
         	String phone = request.getParameter("phone");
         	
         	
-        	
         	book.addEntry(firstName, phone);
+        
         	
+    
         	response.getWriter().println("Added " + firstName + ", there are now " + book.size() + "entries" );
         }
 }
