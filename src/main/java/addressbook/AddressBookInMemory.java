0d0d200e0da09212f989292caada46dabba8a708
Zdzichu <Zdzichu@LAPTOP-ATQM63LV>
2020-03-16 11:55:11 +0100

    memory

--- src/main/java/addressbook/AddressBookInMemory.java
+++ src/main/java/addressbook/AddressBookInMemory.java
@@ -26,6 +26,6 @@ public class AddressBookInMemory implements AddressBook {
 	public void addEntry(String firstName, String phone, String surname, String country, String city) {
 		// TODO Auto-generated method stub
 		
-
+	}
 
 }
