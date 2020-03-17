4b2a9f1c2bad096adb1767138087c5f4478d08b0
Zdzichu <Zdzichu@LAPTOP-ATQM63LV.mshome.net>
2020-03-17 16:24:16 +0100

    gender1

--- src/main/java/addressbook/AddressBookServlet.java
+++ src/main/java/addressbook/AddressBookServlet.java
@@ -20,8 +20,8 @@ import javax.servlet.http.HttpServletResponse;
         	String firstName = request.getParameter("firstName");
         	String city = request.getParameter("city");
         	String country = request.getParameter("country");
-        	String malegender = request.getParameter("malegender");
         	
+        	String gender = request.getParameter("gender");
         	String[] myStringArray = new String[] 
             { request.getParameter("firstName"), 
               request.getParameter("phone"), 
@@ -30,7 +30,7 @@ import javax.servlet.http.HttpServletResponse;
                	
         	book.addEntry(surname, myStringArray);   
     
-        	response.getWriter().println("Added " + malegender + ", Added " + surname + ", Added " + firstName + " Added " + phone + " Added " + city + " Added " + country +", there are now " + book.size() + " entries" );
+        	response.getWriter().println("Added " + gender + ", Added " + surname + ", Added " + firstName + " Added " + phone + " Added " + city + " Added " + country +", there are now " + book.size() + " entries" );
         	       	
         	
         }
