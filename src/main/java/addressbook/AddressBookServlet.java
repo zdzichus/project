981d79eca8d347bcba18fa6f91c1b07175c5bd46
Zdzichu <Zdzichu@LAPTOP-ATQM63LV>
2020-03-22 13:03:02 +0100

    upd16

--- src/main/java/addressbook/AddressBookServlet.java
+++ src/main/java/addressbook/AddressBookServlet.java
@@ -1,7 +1,6 @@
 package addressbook;
 
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
@@ -18,55 +17,57 @@ import javax.servlet.http.HttpServletResponse;
 	public static final long serialVersionUID = 1L;
 		AddressBook book = new AddressBookMongo();
 		
-        public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         	 response.setContentType("text/html; charset=UTF-8");
-        	 PrintWriter out = response.getWriter();
+        	 
         	
-         	String person_table ="PERSON";
-        	String surname = request.getParameter("surname");
-        	String phone = request.getParameter("phone");
-        	String firstName = request.getParameter("firstName");
-        	String city = request.getParameter("city");
-        	String country = request.getParameter("country");
-        	String select = request.getParameter("gender"); 
+         	 String person_table ="PERSON";
+        	 String surname = request.getParameter("surname");
+        	 String phone = request.getParameter("phone");
+        	 String firstName = request.getParameter("firstName");
+        	 String city = request.getParameter("city");
+        	 String country = request.getParameter("country");
+        	 String select = request.getParameter("gender"); 
         	   if (surname.isEmpty() || phone.isEmpty() || firstName.isEmpty() ||
         		   city.isEmpty() || country.isEmpty() )  
         	   {
         		   RequestDispatcher req = request.getRequestDispatcher("index.jsp");
        			   req.include(request, response);
+                         	
         	   }
         	   else
         	   {
+        		   String[] myStringArray = new String[] 
+       		        { 
+       		        request.getParameter("surname"),		
+       		        request.getParameter("firstName"), 
+       		        request.getParameter("phone"), 
+       		        request.getParameter("city"),
+       		        request.getParameter("country"),
+       		        request.getParameter("gender")};
         		   
-        		    
-        		   
-        		  String[] myStringArray = new String[] 
-        		            { 
-        		             request.getParameter("surname"),		
-        		             request.getParameter("firstName"), 
-        		             request.getParameter("phone"), 
-        		             request.getParameter("city"),
-        		             request.getParameter("country"),
-        		             request.getParameter("gender")}; 
-        		             book.addEntry(person_table, myStringArray);
-        		             try {
-        		             	 out.println("<p> Wsztskie wpisy " + (book.readAllEntry()) + ")</p>");
-					                       
-        		             
-        		             }finally {out.close();
-        		             }
-        		             RequestDispatcher req = request.getRequestDispatcher("displayForm.jsp");
-        		             req.forward(request, response);
-        		             System.out.print( book.readEntry());	 
-        		            
-        		            
+       		        book.addEntry(person_table, myStringArray);
+        		            		              
 
         	   }         
         		         
  			         	   
-           }     
+       }     
+        public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        	PrintWriter out = response.getWriter();
+       	    response.setContentType("text/html; charset=UTF-8");
+            try {
+        	    out.println("<p> Wsztskie wpisy " + (book.readAllEntry()) + ")</p>");
+                      
+        
+        }finally {out.close();
+        }
+        RequestDispatcher req = request.getRequestDispatcher("displayForm.jsp");
+        req.forward(request, response);
+        System.out.print( book.readEntry());	 
+        }
         	   
         	   
-        }    	
+ }    	
 
  
