cd528e8ec70f42f0ee30cd82d3ae2150a00d5afc
Zdzichu <Zdzichu@LAPTOP-ATQM63LV>
2020-03-22 13:32:34 +0100

    upd19

--- src/main/java/addressbook/AddressBookServlet.java
+++ src/main/java/addressbook/AddressBookServlet.java
@@ -14,7 +14,7 @@ public class AddressBookServlet extends HttpServlet {
 	public static final long serialVersionUID = 1L;
 	AddressBook book = new AddressBookMongo();
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 
