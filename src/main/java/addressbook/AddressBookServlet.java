f05842fd51e6589e324f631537b823fb36a1b2c5
Zdzichu <Zdzichu@LAPTOP-ATQM63LV>
2020-03-24 17:41:48 +0100

    master29

--- src/main/java/addressbook/AddressBookServlet.java
+++ src/main/java/addressbook/AddressBookServlet.java
@@ -74,10 +74,19 @@ public class AddressBookServlet extends HttpServlet {
 				int countTable = 0;
 				for (int i = 0; i < tempTable.length; i++) {
 					if (wordTable.equals(tempTable[i]))
-	   				countTable++;
-    				out.println("<h2> All of word in  # "  +tempTable[i] +" #</h1> <br>");
-				}
-			   out.println("<h2> All of word sum '' : "  + countTable +"</h1> <br>");
+						System.out.print("wartosc i przed count++"+i);
+						countTable++;
+						System.out.print("wartosc i p0 count++"+i);
+    				    out.println("<h2> All of word in  # "  +tempTable[i] +" #</h1> <br>");
+    				
+    					out.println("<table>"+
+    					"<tr><td>"+tempTable[i+5]+"</td> <td>"+tempTable[i+9]+"</td> <td>"+tempTable[i+11]+"</td> <td>"+tempTable[i+13]+"</td><td>"+tempTable[i+15]+"</td> <td>"+tempTable[i+17]+"</td> <td>"+tempTable[i+19]+"</td></tr>"+
+    					"</table>");
+				    
+				      System.out.print("wartosc i koniec petla"+i);
+					
+				}	
+				
 			   out.println("<table>"+
 			   "<tr><td>"+tempTable[5]+"</td> <td>"+tempTable[9]+"</td> <td>"+tempTable[11]+"</td> <td>"+tempTable[13]+"</td><td>"+tempTable[15]+"</td> <td>"+tempTable[17]+"</td> <td>"+tempTable[19]+"</td></tr>"+
 			   "<tr><td>"+tempTable[25]+"</td> <td>"+tempTable[29]+"</td> <td>"+tempTable[31]+"</td> <td>"+tempTable[33]+"</td><td>"+tempTable[35]+"</td> <td>"+tempTable[37]+"</td> <td>"+tempTable[39]+"</td></tr>"+
