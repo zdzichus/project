483227e7d8fdb8ba99f1fa4c77c3884fa770cfe4
Zdzichu <Zdzichu@LAPTOP-ATQM63LV>
2020-03-30 17:51:54 +0200

    m70

--- src/main/java/addressbook/AddressBookServlet.java
+++ src/main/java/addressbook/AddressBookServlet.java
@@ -62,33 +62,33 @@ public class AddressBookServlet extends HttpServlet {
 			}
 				out.println("<!DOCTYPE html>");
 				out.println("<html><head><meta charset=\"UTF-8\">\r\n" + 
-						"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n" + 
-						"<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\r\n" + 
-						"<link rel=\"stylesheet\"\r\n" + 
+						"	<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n" + 
+						"	<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\" />\r\n" + 
+						"	<link rel=\"stylesheet\"\r\n" + 
 						"	href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css\"\r\n" + 
 						"	integrity=\"sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh\"\r\n" + 
 						"	crossorigin=\"anonymous\">\r\n" + 
-						"<script src=\"https://code.jquery.com/jquery-3.4.1.slim.min.js\"\r\n" + 
+						"	<script src=\"https://code.jquery.com/jquery-3.4.1.slim.min.js\"\r\n" + 
 						"	integrity=\"sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n\"\r\n" + 
 						"	crossorigin=\"anonymous\"></script>\r\n" + 
-						"<script\r\n" + 
+						"	<script\r\n" + 
 						"	src=\"https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js\"\r\n" + 
 						"	integrity=\"sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo\"\r\n" + 
 						"	crossorigin=\"anonymous\"></script>\r\n" + 
-						"<script\r\n" + 
+						"	<script\r\n" + 
 						"	src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js\"\r\n" + 
 						"	integrity=\"sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6\"\r\n" + 
 						"	crossorigin=\"anonymous\"></script>\r\n" + 
-						"<title>Application results </title></head>");
+						"	<title>Application results </title></head>");
 				out.println("<body><div class=\"navbar navbar-expand-lg fixed-top navbar-dark bg-primary\">\r\n" + 
-						"		<div class=\"container\">\r\n" + 
+						"			<div class=\"container\">\r\n" + 
 						"			<a class=\"navbar-brand\" href=\"../\">Application</a>\r\n" + 
 						"			<button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\"\r\n" + 
-						"				data-target=\"#navbarColor01\" aria-controls=\"navbarColor01\"\r\n" + 
-						"				aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n" + 
-						"				<span class=\"navbar-toggler-icon\"></span>\r\n" + 
+						"			data-target=\"#navbarColor01\" aria-controls=\"navbarColor01\"\r\n" + 
+						"			aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n" + 
+						"			<span class=\"navbar-toggler-icon\"></span>\r\n" + 
 						"			</button>\r\n" + 
-						"\r\n" + 
+						"			\r\n" + 
 						"			<div class=\"collapse navbar-collapse\" id=\"navbarColor01\">\r\n" + 
 						"				<ul class=\"navbar-nav mr-auto\">\r\n" + 
 						"					<li class=\"nav-item active\"><a class=\"nav-link\" href=\"#\">Home\r\n" + 
@@ -110,12 +110,16 @@ public class AddressBookServlet extends HttpServlet {
 						"		</div>\r\n" + 
 						"	</div>");
 				out.println("<div class=\"container\">\r\n" + 
-						"\r\n" + 
+						"		\r\n" + 
 						"		<div class=\"row\">\r\n" + 
-						"			<div class=\"col-lg-5\"><br><br><br><br><h2> There are now: " + book.size() + " entries.<br> Male: "+ countMale +" Females: " +countFemale +"</h1> <br>");
-				out.println(
-						"  <table class=\"table table-hover\">\r\n" + 
-						"  <thead>\r\n" + 
+						"		<div class=\"col-lg-5\"><br><br><br><br>" +
+						"        <div class=\"alert alert-dismissible alert-warning\">"+
+						"        <button type=\"button\" class=\"close\" data-dismiss=\"alert\">&times;</button>" + 
+						"          <h4 class=\"alert-heading\">Database entries: " + book.size() + " </h4>" +
+						"          <h5 class=\"alert-heading\">Males: " + countMale + " Females: " +countFemale +" </h4></div>" );    
+					//	"       <h2> There are now: " + book.size() + " entries.<br> Male: "+ countMale +" Females: " +countFemale +"</h1> <br>");
+				out.println("<table class=\"table table-hover\">\r\n" + 
+						"   <thead>\r\n" + 
 						"    <tr>\r\n" + 
 						"      <th scope=\"col\">Surname</th>\r\n" + 
 						"      <th scope=\"col\">Name</th>\r\n" + 
